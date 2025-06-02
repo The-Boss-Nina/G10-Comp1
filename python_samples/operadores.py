5861c8c84055cfd84d0bfa77713ac6c360b1285a
Matheus Silverio <matheusphillipo@gmail.com>
2025-06-02 01:07:47 -0300

    caso de teste operadores aritmeticos e comparacao OK

---
+++ python_samples/operadores.py
@@ -0,0 +1,26 @@
+# Teste de operadores aritméticos
+a = 10
+b = 3
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+modulo = a % b
+potencia = a ** b
+divisao_inteira = a // b
+
+print(soma)
+print(subtracao)
+print(multiplicacao)
+
+# Operadores de comparação
+maior = a > b
+menor = a < b
+igual = a == b
+diferente = a != b
+maior_igual = a >= b
+menor_igual = a <= b
+
+print(maior)
+print(igual)
