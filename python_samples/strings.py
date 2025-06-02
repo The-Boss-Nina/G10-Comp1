7c3d16a4899b5c6a7d18065345b41e2722f748f8
Matheus Silverio <matheusphillipo@gmail.com>
2025-06-02 01:08:20 -0300

    caso de teste strings OK

---
+++ python_samples/strings.py
@@ -0,0 +1,24 @@
+# Teste de strings
+
+# Strings simples
+nome = "João"
+sobrenome = 'Silva'
+frase = "Olá, mundo!"
+
+print(nome)
+print(sobrenome)
+print(frase)
+
+# Strings com aspas
+texto_aspas_duplas = "Ele disse: 'Olá!'"
+texto_aspas_simples = 'Ela respondeu: "Oi!"'
+
+print(texto_aspas_duplas)
+print(texto_aspas_simples)
+
+# Strings vazias
+vazia_dupla = ""
+vazia_simples = ''
+
+print(vazia_dupla)
+print(vazia_simples)
