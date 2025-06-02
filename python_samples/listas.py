26711abe9178ea66a26ee46c2cf56f93f0437947
Matheus Silverio <matheusphillipo@gmail.com>
2025-06-02 01:05:25 -0300

    caso de teste listas OK

---
+++ python_samples/listas.py
@@ -0,0 +1,25 @@
+# Teste de listas
+
+# Lista vazia
+lista_vazia = []
+
+# Lista com elementos
+numeros = [1, 2, 3, 4, 5]
+nomes = ["Ana", "Bruno", "Carlos"]
+
+# Lista mista
+mista = [1, "texto", 3, "outro"]
+
+print(numeros)
+print(nomes)
+
+# Acesso a elementos (se suportado)
+primeiro = numeros[0]
+ultimo = numeros[4]
+
+print(primeiro)
+print(ultimo)
+
+# Lista aninhada
+matriz = [[1, 2], [3, 4], [5, 6]]
+print(matriz)
