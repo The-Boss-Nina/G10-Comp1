88deef1f5a8b84824a317f24db9d21f6d733c437
Matheus Silverio <matheusphillipo@gmail.com>
2025-06-02 01:05:56 -0300

    caso de teste operadores logicos OK

---
+++ python_samples/logicos.py
@@ -0,0 +1,19 @@
+# Teste de operadores lógicos
+verdadeiro = True
+falso = False
+
+resultado_and = verdadeiro and falso
+resultado_or = verdadeiro or falso
+resultado_not = not falso
+
+print(resultado_and)
+print(resultado_or)
+print(resultado_not)
+
+# Combinações mais complexas
+x = 5
+y = 10
+z = 15
+
+complexo = (x > 3) and (y < 20) or (z == 15)
+print(complexo)
